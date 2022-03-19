d0b3f333f7b84ba3590315ff8621bb3421a5602c
Anita Situmorang <93483591+Situmorang2109@users.noreply.github.com>
2022-03-19 14:08:21 +0700

    Add files via upload

---
+++ Penjumlahan.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302194043
+{
+    internal class Penjumlahan
+    {
+
+            PenjumlahanGeneric<string> penjumlahan = new PenjumlahanGeneric<string>();
+            String x = "Anita Situmorang";
+            penjumlahan.JumlahAngka(x);
+
+        }
+    }
+    public class PenjumlahanGeneric<T>
+    {
+        public void JumlahAngka(T x)
+        {
+            Console.WriteLine("Jumlah Tiga Angka " + x);
+
+            DataGeneric<String> data = new DataGeneric<string>("1302194043");
+            data.PrintData();
+        }
+    }
+
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Jumlah Angka yang tersimpan adalah: " + this.data);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302194043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KodeFilm table_KodeFilm = new KodeFilm();
+            Console.WriteLine("=== GET KODE FILM ===");
+            table_KodeFilm.getKodeFilm("AADC");
+            Console.WriteLine("==== GET ALL KODE FILM ====");
+            table_KodeFilm.getAllkodeFilm();
+
+        }
+
+    }
+    class KodeFilm
+    {
+
+        Dictionary<string, string> dic = new Dictionary<string, string>() {
+            {"AADC", "A10"},
+            {"Dear Nathan","D11" },
+            {"Kukira Kau Rumah"  ,"K12" },
+            {"Sobat Ambyar"    ,"S13" },
+            {"June dan Kopi"  ,"J14"},
+            {"Affliction" ,"A15" },
+            {"Guru-guru Gokil","G16"},
+            {"Dua Garis Biru" ,"D17" }, };
+
+        public void getKodeFilm(string kel)
+        {
+
+            if (dic.ContainsKey(kel))
+            {
+                Console.WriteLine(kel + " : " + dic[kel]);
+            }
+            else
+            {
+                Console.WriteLine(kel + " tidak ditemukan");
+            }
+        }
+
+        public void getAllkodeFilm()
+        {
+            foreach (KeyValuePair<string, string> ele1 in dic)
+            {
+                Console.WriteLine("{0} \t\t {1}", ele1.Key, ele1.Value);
+            }
+        }
+    }
+}
